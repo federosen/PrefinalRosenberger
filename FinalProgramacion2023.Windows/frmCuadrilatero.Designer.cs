485fbcc039742e38562cd9b391196058eef32204
federosen <139183414+federosen@users.noreply.github.com>
2024-02-28 12:44:12 -0300

    Add files via upload

---
+++ FinalProgramacion2023.Windows/frmCuadrilatero.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace FinalProgramacion2023.Windows
+{
+    partial class frmCuadrilatero
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            groupBox1 = new GroupBox();
+            rbtPuntos = new RadioButton();
+            rbtRayas = new RadioButton();
+            rbtLineal = new RadioButton();
+            cboRelleno = new ComboBox();
+            label4 = new Label();
+            btnCancelar = new Button();
+            btnOK = new Button();
+            txtLadoB = new TextBox();
+            label2 = new Label();
+            txtLadoA = new TextBox();
+            label1 = new Label();
+            errorProvider1 = new ErrorProvider(components);
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rbtPuntos);
+            groupBox1.Controls.Add(rbtRayas);
+            groupBox1.Controls.Add(rbtLineal);
+            groupBox1.Location = new Point(41, 144);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(284, 76);
+            groupBox1.TabIndex = 16;
+            groupBox1.TabStop = false;
+            groupBox1.Text = " Bordes ";
+            // 
+            // rbtPuntos
+            // 
+            rbtPuntos.AutoSize = true;
+            rbtPuntos.Location = new Point(168, 29);
+            rbtPuntos.Name = "rbtPuntos";
+            rbtPuntos.Size = new Size(62, 19);
+            rbtPuntos.TabIndex = 0;
+            rbtPuntos.TabStop = true;
+            rbtPuntos.Text = "Puntos";
+            rbtPuntos.UseVisualStyleBackColor = true;
+            // 
+            // rbtRayas
+            // 
+            rbtRayas.AutoSize = true;
+            rbtRayas.Location = new Point(89, 29);
+            rbtRayas.Name = "rbtRayas";
+            rbtRayas.Size = new Size(55, 19);
+            rbtRayas.TabIndex = 0;
+            rbtRayas.TabStop = true;
+            rbtRayas.Text = "Rayas";
+            rbtRayas.UseVisualStyleBackColor = true;
+            // 
+            // rbtLineal
+            // 
+            rbtLineal.AutoSize = true;
+            rbtLineal.Location = new Point(15, 29);
+            rbtLineal.Name = "rbtLineal";
+            rbtLineal.Size = new Size(56, 19);
+            rbtLineal.TabIndex = 0;
+            rbtLineal.TabStop = true;
+            rbtLineal.Text = "Lineal";
+            rbtLineal.UseVisualStyleBackColor = true;
+            // 
+            // cboRelleno
+            // 
+            cboRelleno.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboRelleno.FormattingEnabled = true;
+            cboRelleno.Items.AddRange(new object[] { "Rojo", "Verde", "Azul" });
+            cboRelleno.Location = new Point(101, 115);
+            cboRelleno.Name = "cboRelleno";
+            cboRelleno.Size = new Size(121, 23);
+            cboRelleno.TabIndex = 15;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(41, 118);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 15);
+            label4.TabIndex = 14;
+            label4.Text = "Relleno:";
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(250, 229);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 61);
+            btnCancelar.TabIndex = 12;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click;
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new Point(41, 229);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(75, 61);
+            btnOK.TabIndex = 13;
+            btnOK.Text = "OK";
+            btnOK.UseVisualStyleBackColor = true;
+            btnOK.Click += btnOK_Click;
+            // 
+            // txtLadoB
+            // 
+            txtLadoB.Location = new Point(101, 51);
+            txtLadoB.Name = "txtLadoB";
+            txtLadoB.Size = new Size(100, 23);
+            txtLadoB.TabIndex = 10;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Lado B;";
+            // 
+            // txtLadoA
+            // 
+            txtLadoA.Location = new Point(101, 22);
+            txtLadoA.Name = "txtLadoA";
+            txtLadoA.Size = new Size(100, 23);
+            txtLadoA.TabIndex = 11;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Lado A:";
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // frmCuadrilatero
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(373, 313);
+            Controls.Add(groupBox1);
+            Controls.Add(cboRelleno);
+            Controls.Add(label4);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnOK);
+            Controls.Add(txtLadoB);
+            Controls.Add(label2);
+            Controls.Add(txtLadoA);
+            Controls.Add(label1);
+            Name = "frmCuadrilatero";
+            Text = "frmCuadrilatero";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton rbtPuntos;
+        private RadioButton rbtRayas;
+        private RadioButton rbtLineal;
+        private ComboBox cboRelleno;
+        private Label label4;
+        private Button btnCancelar;
+        private Button btnOK;
+        private TextBox txtLadoB;
+        private Label label2;
+        private TextBox txtLadoA;
+        private Label label1;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ FinalProgramacion2023.Windows/frmPrincipal.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace FinalProgramacion2023.Windows
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            toolStrip1 = new ToolStrip();
+            tsbNuevo = new ToolStripButton();
+            tsbBorrar = new ToolStripButton();
+            tsbEditar = new ToolStripButton();
+            toolStripSeparator1 = new ToolStripSeparator();
+            tsbFiltrar = new ToolStripButton();
+            tsbActualizar = new ToolStripButton();
+            toolStripSeparator2 = new ToolStripSeparator();
+            tsbOrdenar = new ToolStripButton();
+            toolStripSeparator3 = new ToolStripSeparator();
+            tsbSalir = new ToolStripButton();
+            splitContainer1 = new SplitContainer();
+            dgvDatos = new DataGridView();
+            txtCantidad = new TextBox();
+            label1 = new Label();
+            ColumnaLadoA = new DataGridViewTextBoxColumn();
+            ColumnaLadoB = new DataGridViewTextBoxColumn();
+            ColumnaRelleno = new DataGridViewTextBoxColumn();
+            ColumnaArea = new DataGridViewTextBoxColumn();
+            ColumnaPerimetro = new DataGridViewTextBoxColumn();
+            toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).BeginInit();
+            SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { tsbNuevo, tsbBorrar, tsbEditar, toolStripSeparator1, tsbFiltrar, tsbActualizar, toolStripSeparator2, tsbOrdenar, toolStripSeparator3, tsbSalir });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 54);
+            toolStrip1.TabIndex = 0;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbNuevo
+            // 
+            tsbNuevo.Image = Properties.Resources.new_copy_32px;
+            tsbNuevo.ImageScaling = ToolStripItemImageScaling.None;
+            tsbNuevo.ImageTransparentColor = Color.Magenta;
+            tsbNuevo.Name = "tsbNuevo";
+            tsbNuevo.Size = new Size(46, 51);
+            tsbNuevo.Text = "Nuevo";
+            tsbNuevo.TextImageRelation = TextImageRelation.ImageAboveText;
+            tsbNuevo.Click += tsbNuevo_Click;
+            // 
+            // tsbBorrar
+            // 
+            tsbBorrar.Image = Properties.Resources.delete_file_32px;
+            tsbBorrar.ImageScaling = ToolStripItemImageScaling.None;
+            tsbBorrar.ImageTransparentColor = Color.Magenta;
+            tsbBorrar.Name = "tsbBorrar";
+            tsbBorrar.Size = new Size(43, 51);
+            tsbBorrar.Text = "Borrar";
+            tsbBorrar.TextImageRelation = TextImageRelation.ImageAboveText;
+            tsbBorrar.Click += tsbBorrar_Click;
+            // 
+            // tsbEditar
+            // 
+            tsbEditar.Image = Properties.Resources.edit_file_32px;
+            tsbEditar.ImageScaling = ToolStripItemImageScaling.None;
+            tsbEditar.ImageTransparentColor = Color.Magenta;
+            tsbEditar.Name = "tsbEditar";
+            tsbEditar.Size = new Size(41, 51);
+            tsbEditar.Text = "Editar";
+            tsbEditar.TextImageRelation = TextImageRelation.ImageAboveText;
+            tsbEditar.Click += tsbEditar_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 54);
+            // 
+            // tsbFiltrar
+            // 
+            tsbFiltrar.Image = Properties.Resources.filled_filter_32px;
+            tsbFiltrar.ImageScaling = ToolStripItemImageScaling.None;
+            tsbFiltrar.ImageTransparentColor = Color.Magenta;
+            tsbFiltrar.Name = "tsbFiltrar";
+            tsbFiltrar.Size = new Size(41, 51);
+            tsbFiltrar.Text = "Filtrar";
+            tsbFiltrar.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // tsbActualizar
+            // 
+            tsbActualizar.Image = Properties.Resources.refresh_32px;
+            tsbActualizar.ImageScaling = ToolStripItemImageScaling.None;
+            tsbActualizar.ImageTransparentColor = Color.Magenta;
+            tsbActualizar.Name = "tsbActualizar";
+            tsbActualizar.Size = new Size(63, 51);
+            tsbActualizar.Text = "Actualizar";
+            tsbActualizar.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(6, 54);
+            // 
+            // tsbOrdenar
+            // 
+            tsbOrdenar.Image = Properties.Resources.sorting_arrows_32px;
+            tsbOrdenar.ImageScaling = ToolStripItemImageScaling.None;
+            tsbOrdenar.ImageTransparentColor = Color.Magenta;
+            tsbOrdenar.Name = "tsbOrdenar";
+            tsbOrdenar.Size = new Size(54, 51);
+            tsbOrdenar.Text = "Ordenar";
+            tsbOrdenar.TextImageRelation = TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator3
+            // 
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new Size(6, 54);
+            // 
+            // tsbSalir
+            // 
+            tsbSalir.Image = Properties.Resources.exit_32px;
+            tsbSalir.ImageScaling = ToolStripItemImageScaling.None;
+            tsbSalir.ImageTransparentColor = Color.Magenta;
+            tsbSalir.Name = "tsbSalir";
+            tsbSalir.Size = new Size(36, 51);
+            tsbSalir.Text = "Salir";
+            tsbSalir.TextImageRelation = TextImageRelation.ImageAboveText;
+            tsbSalir.Click += tsbSalir_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 54);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(dgvDatos);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(txtCantidad);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Size = new Size(800, 396);
+            splitContainer1.SplitterDistance = 330;
+            splitContainer1.TabIndex = 1;
+            // 
+            // dgvDatos
+            // 
+            dgvDatos.AllowUserToAddRows = false;
+            dgvDatos.AllowUserToDeleteRows = false;
+            dgvDatos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDatos.Columns.AddRange(new DataGridViewColumn[] { ColumnaLadoA, ColumnaLadoB, ColumnaRelleno, ColumnaArea, ColumnaPerimetro });
+            dgvDatos.Dock = DockStyle.Fill;
+            dgvDatos.Location = new Point(0, 0);
+            dgvDatos.Name = "dgvDatos";
+            dgvDatos.ReadOnly = true;
+            dgvDatos.RowTemplate.Height = 25;
+            dgvDatos.Size = new Size(800, 330);
+            dgvDatos.TabIndex = 0;
+            // 
+            // txtCantidad
+            // 
+            txtCantidad.Location = new Point(101, 21);
+            txtCantidad.Name = "txtCantidad";
+            txtCantidad.Size = new Size(100, 23);
+            txtCantidad.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Cantidad:";
+            // 
+            // ColumnaLadoA
+            // 
+            ColumnaLadoA.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            ColumnaLadoA.HeaderText = "Lado A:";
+            ColumnaLadoA.Name = "ColumnaLadoA";
+            ColumnaLadoA.ReadOnly = true;
+            // 
+            // ColumnaLadoB
+            // 
+            ColumnaLadoB.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            ColumnaLadoB.HeaderText = "Lado B:";
+            ColumnaLadoB.Name = "ColumnaLadoB";
+            ColumnaLadoB.ReadOnly = true;
+            // 
+            // ColumnaRelleno
+            // 
+            ColumnaRelleno.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            ColumnaRelleno.HeaderText = "Relleno:";
+            ColumnaRelleno.Name = "ColumnaRelleno";
+            ColumnaRelleno.ReadOnly = true;
+            // 
+            // ColumnaArea
+            // 
+            ColumnaArea.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            ColumnaArea.HeaderText = "Area:";
+            ColumnaArea.Name = "ColumnaArea";
+            ColumnaArea.ReadOnly = true;
+            // 
+            // ColumnaPerimetro
+            // 
+            ColumnaPerimetro.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            ColumnaPerimetro.HeaderText = "Perimetro";
+            ColumnaPerimetro.Name = "ColumnaPerimetro";
+            ColumnaPerimetro.ReadOnly = true;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
+            Controls.Add(toolStrip1);
+            Name = "frmPrincipal";
+            Text = "frmPrincipal";
+            Load += frmPrincipal_Load;
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvDatos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ToolStrip toolStrip1;
+        private ToolStripButton tsbNuevo;
+        private ToolStripButton tsbBorrar;
+        private ToolStripButton tsbEditar;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripButton tsbFiltrar;
+        private ToolStripButton tsbActualizar;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripButton tsbOrdenar;
+        private ToolStripSeparator toolStripSeparator3;
+        private ToolStripButton tsbSalir;
+        private SplitContainer splitContainer1;
+        private DataGridView dgvDatos;
+        private TextBox txtCantidad;
+        private Label label1;
+        private DataGridViewTextBoxColumn ColumnaLadoA;
+        private DataGridViewTextBoxColumn ColumnaLadoB;
+        private DataGridViewTextBoxColumn ColumnaRelleno;
+        private DataGridViewTextBoxColumn ColumnaArea;
+        private DataGridViewTextBoxColumn ColumnaPerimetro;
+    }
+}
